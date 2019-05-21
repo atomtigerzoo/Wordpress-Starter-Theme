61e55321cad76b863a87d0aa44dbdb653c782c2a
Henning Stein <stein@atomtigerzoo.com>
2019-05-21 17:18:58 +0200

    corrects the way the constant is checked (Fixes #4)

--- src/inc/config.php
+++ src/inc/config.php
@@ -4,11 +4,13 @@
  * Config
  */
 
-// Development settings
-if(defined(WP_DEBUG) && WP_DEBUG) {
-	// Enable error reporting
+
+/**
+ * Enable errors in WP_DEBUG mode
+ */
+if(defined('WP_DEBUG') && WP_DEBUG) {
 	error_reporting(E_ALL);
 	ini_set('display_errors', '1');
 } else {
-	
+	// do something here in production mode
 }
