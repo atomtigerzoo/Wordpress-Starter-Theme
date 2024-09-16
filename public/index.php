69a6f86fd54f30547b9df96970d4834b8c551935
Henning <stein@atomtigerzoo.com>
2024-09-16 11:26:38 +0200

    other: creates image placeholder folder

--- public/index.php
+++ public/index.php
@@ -5,8 +5,8 @@
 get_header();
 
 ?>
-<div class="mt-12">
-	<h1 class="font-semibold text-3xl text-center text-sky-700">✨ WP Starter Theme ✨</h1>
+<div class="mt-12 text-center">
+	<h1 class="font-semibold text-3xl text-sky-700">✨ WP Starter Theme ✨</h1>
 </div>
 
 <?php
