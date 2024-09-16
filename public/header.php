8ccd7a9c1f2826404342763371f0cf0632f72f5b
Henning <stein@atomtigerzoo.com>
2024-09-16 09:46:32 +0200

    feat: adds a placeholder favicon

--- public/header.php
+++ public/header.php
@@ -11,5 +11,7 @@
 	// WP Head
 	wp_head();
 	?>
+	<link rel="icon" type="image/png" sizes="32x32" href="<?= get_template_directory_uri() ?>/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="<?= get_template_directory_uri() ?>/favicon-16x16.png">
 </head>
 <body <?= body_class() ?>>
